0694b4096b2b73a54a3c3641a3eedf4c00c500fa
Rana Gaber <ranagaber810@gmail.com>
2023-11-06 14:35:26 +0200

    init

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,15 @@ from flask import Flask
 from app.config import Config
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy 
+#################################################
+
+
 app=Flask(__name__)
 app.config.from_object(Config)
 db=SQLAlchemy(app)
 api = Api(app)
-from app.models import category,user
+
+
+#######################################################3
+from app.models.user import User
+from app.models.category import Category
