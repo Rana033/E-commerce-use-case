595827fc8ac2c6bcefbc0376c5e316777b48c29c
Rana Gaber <ranagaber810@gmail.com>
2023-11-13 00:39:10 +0200

    feat:login

--- app/resources/user_resource.py
+++ app/resources/user_resource.py
@@ -1,5 +1,5 @@
 from flask import jsonify , request
-from flask_restful import Resource
+from flask_restful import Resource,abort
 from app import db,api,jwt
 from app.models.user import User
 from passlib.hash import sha256_crypt
@@ -12,7 +12,7 @@ class RegistrationUser(Resource):
         data = request.get_json()
         password=sha256_crypt.hash(data['password'])
         del data['password']
-        new_user = User(**data, password_hash=password)
+        new_user = User(**data, password=password)
         try:
             access_token = create_access_token(identity=new_user.id)
             db.session.add(new_user)
@@ -26,11 +26,25 @@ class RegistrationUser(Resource):
         
  
 
-    
-    
+class UserAll(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+
+            user_log = db.session.query(User).filter(User.user_email==data['user_email']).first()
+            print(user_log)
+            if user_log and sha256_crypt.verify(data['password'],user_log.password):
+                access_token = create_access_token(identity=user_log.id)
+                return jsonify({'message': 'Login successful', 'access_token': access_token}, 200)
+            else:
+                return jsonify({'message':'invalid email or password'},401)
+
+        except Exception as e:
+            abort(400, message=str(e))
     
     
     
     
     
 api.add_resource(RegistrationUser, '/user')
+api.add_resource(UserAll, '/user/log')
